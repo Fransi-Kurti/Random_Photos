f022f2fd6311095a939bc9b5fb0d943f79f93d67
fransi-kurti <fransi.kurti@gmail.com>
2023-02-18 20:43:52 +0100

    random photos project javascript update

---
+++ index.js
@@ -0,0 +1,8 @@
+const mainContainer = document.getElementById("container");
+
+const btnEl = document.getElementById("btn");
+
+
+btnEl.addEventListener("click", ()=>{
+    console.log("clicked");
+});
